3e1615c747968ecdcfa48025ab0dcb3c0b52b7a9
angeloey <angelomaoudis@gmail.com>
2024-01-29 20:03:56 +0000

    talks to HC-06

--- SerialCommands.py
+++ SerialCommands.py
@@ -1,6 +1,6 @@
 import serial
 
-serialPort = serial.Serial(port = "COM3", baudrate=9600,
+serialPort = serial.Serial(port = "COM10", baudrate=9600,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # setup serial port on pc
 
 serialString = ""                                           # Used to hold data coming over UART
